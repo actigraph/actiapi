3212aa47c6a74ce264c8b8d14a56e547dc9cb634
Dawid Gerstel <dawid.gerstel@theactigraph.com>
2023-04-14 18:14:47 +0200

    Enable to pull raw data between dates

--- tests/test_v3.py
+++ tests/test_v3.py
@@ -51,8 +51,30 @@ def test_daily(v3_client, v3_study_id):
     }
 
 
-def test_raw(v3_client, v3_study_id):
-    metadata = v3_client.get_study_metadata(v3_study_id)
-    id_ = metadata[0]["id"]
-    raw_files = v3_client.get_files(user=id_, study_id=v3_study_id)
-    assert len(raw_files) == 0
+class TestRaw:
+    def test_raw(self, v3_client, v3_study_id):
+        metadata = v3_client.get_study_metadata(v3_study_id)
+        id_ = metadata[0]["id"]
+        raw_files = v3_client.get_files(user=id_, study_id=v3_study_id)
+        assert len(raw_files) == 0
+
+
+    def test_raw_between_dates(self, v3_client, v3_study_id):
+        metadata = v3_client.get_study_metadata(v3_study_id)
+        id_ = metadata[0]["id"]
+        raw_files = v3_client.get_files(user=id_, study_id=v3_study_id, start="2010-04-14", end="2022-04-15")
+        assert len(raw_files) == 0
+
+
+    def test_raw_before_end(self, v3_client, v3_study_id):
+        metadata = v3_client.get_study_metadata(v3_study_id)
+        id_ = metadata[0]["id"]
+        raw_files = v3_client.get_files(user=id_, study_id=v3_study_id, end="2022-04-15")
+        assert len(raw_files) == 0
+
+
+    def test_raw_after_start(self, v3_client, v3_study_id):
+        metadata = v3_client.get_study_metadata(v3_study_id)
+        id_ = metadata[0]["id"]
+        raw_files = v3_client.get_files(user=id_, study_id=v3_study_id, start="2022-04-15")
+        assert len(raw_files) == 0
